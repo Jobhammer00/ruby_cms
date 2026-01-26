f6d8df53d6c03fe5e71fd2267df334d32f24e3bd
Job Hammer <job.hammer@moneybird.com>
2026-01-26 15:23:55 +0100

    Initial Commit

---
+++ spec/ruby_cms_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyCms do
+  it "has a version number" do
+    expect(RubyCms::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
